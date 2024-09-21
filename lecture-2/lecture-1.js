8bf1c2e353048797065bb4a4430ef9c15cb7ad53
Vansh Dhameliya <vanshdhameliya07@gmail.com>
2024-09-21 10:08:37 +0530

    JavaScript

---
+++ lecture-2/lecture-1.js
@@ -0,0 +1,14 @@
+console.log(`hello`);
+document.write(`<h1>hello</h1>`);
+alert(`hi kemm cho !!!`);
+document.getElementById(`msg`).innerHTML = "hiiii";
+
+var a = 10, b = 5;
+
+console.log(`A sum ${a + b}`);
+document.getElementById(`msg`).innerHTML = `sum =  ${a + b}`;
+
+
+var a =true;
+console.log(`A = ${a}`);
+console.log(typeof(a));
